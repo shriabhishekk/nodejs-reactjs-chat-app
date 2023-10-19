b9dd73e29ad3fd89a869c60fb08486f987fe8f7f
Shri Abhishek Choudhary <92842067+shriabhishekk@users.noreply.github.com>
2023-10-19 21:50:49 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try{
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers:{"private-key":"b299db05-7b7e-436e-a5d3-e8cf84efa946"}}
+    )
+    return res.status(r.status).json(r.data )
+  }
+  catch(e){
+    // return res.status(e.response.status).json(e.response.data )
+  }
+
+  
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
